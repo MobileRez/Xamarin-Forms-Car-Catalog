6352d585275f5af56d3213902b508da53ee5b816
MobileRez <mitchmuenster@gmail.com>
2015-07-03 09:48:41 -0500

    Initial commit of project files.
    Initial commit of project files to get the project started. Initial
    Commit provides a basic layout of the app and structure of high level
    root navigation items which are vehicle classes (Car, Truck, and SUV).

---
+++ XamarinFormsCarCatalog/XamarinFormsCarCatalog/Views/BaseView.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using XamarinFormsCarCatalog.ViewModel;
+
+namespace XamarinFormsCarCatalog.Views
+{
+    public class BaseView : ContentPage
+    {
+        public BaseView()
+        {
+            SetBinding(TitleProperty, new Binding(BaseViewModel.TitlePropertyName));
+            SetBinding(IconProperty, new Binding(BaseViewModel.IconPropertyName));
+        }
+    }
+}
